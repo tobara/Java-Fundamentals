f597b69ac4ed2072260f123ff71572a3ef19dc1c
Tim OBara <t_obara@Admins-MacBook-Pro.local>
2016-10-20 16:30:10 -0400

    many

--- Value.java
+++ Value.java
@@ -1,3 +1,7 @@
+// Your task is to make two functions, max and min (maximum and minimum in PHP)
+// that take a(n) array/vector of integers list as input and outputs, respectively,
+// the largest and lowest number in that array/vector
+
 public class Value {
 
   public int min(int[] list) {
--- Vowels.java
+++ Vowels.java
@@ -1,3 +1,7 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, and u as vowels for this Kata.
+
 public class Vowels {
 
   public static int getCount(String str) {
